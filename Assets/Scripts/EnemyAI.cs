760a2dafaeb1a7ce05a3f0418f0a3cf4dbc3de72
Federico Fasce <federico@urustar.net>
2014-10-01 11:28:03 +0200

    Added wandering enemy units

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+	private Transform _terrain;
+	private MovementManager _movement;
+	private float _timer=0;
+	private bool _isEngaged=false;
+	private float _stopTimer;
+	// Use this for initialization
+	void Start () {
+		_movement = GetComponent<MovementManager>();
+		_terrain = GameObject.FindWithTag("Ground").transform;
+		_stopTimer = Random.Range(1,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_isEngaged) {
+
+		}
+		else {
+			_timer+=Time.deltaTime;
+			if (_timer >= _stopTimer) {
+				float positionX = Random.Range ((int)_terrain.collider.bounds.min.x, (int)_terrain.collider.bounds.max.x);
+				float positionZ = Random.Range ((int)_terrain.collider.bounds.min.z, (int)_terrain.collider.bounds.max.z);
+				_movement.MoveToTarget(new Vector3(positionX, transform.position.y, positionZ));
+				_timer=0;
+				_stopTimer = Random.Range(1,5);
+			}
+
+		}
+	}
+
+	void ChangeBehavior() {
+
+	}
+
+
+}
--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -2,15 +2,24 @@
 using System.Collections;
 
 public class MovementManager : MonoBehaviour {
-
+	public bool isMoving;
 	public float moveSpeed;
 
 	public void MoveToTarget(Vector3 target) {
-		StopAllCoroutines();
+		if (isMoving)
+		{
+			StopMoving();
+		}
 		StartCoroutine (ReachTarget(target));
+	}
+
+	public void StopMoving() {
+		StopAllCoroutines();
+		isMoving=false;
 	}
 	
 	IEnumerator ReachTarget(Vector3 target) {
+		isMoving=true;
 		while (transform.position != target)
 		{
 			transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
